e199b3fab52deeb186dfbc33a3ce669ec141ab36
daniil <daniilzhilyaev@gmail.com>
2023-07-28 16:27:40 +0300

    add switch example

--- 1_sqitch_case/Program.cs
+++ 1_sqitch_case/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите день недели: ");
+int number = int.Parse(Console.ReadLine()!);
+
+switch (number)
+{
+    case 6:{
+        Console.WriteLine("Это суббота");
+        break;
+    }
+     case 7:{
+        Console.WriteLine("Это воскресенье");
+        break;
+    }
+    default:{
+        Console.WriteLine("Это будний день");
+        break;
+    }
+}
